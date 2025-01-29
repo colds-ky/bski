88123e4f2b8b6f9b83ab641983b41f6e4faef2f9
Oleg Mihailik <mihailik@gmail.com>
2025-01-29 22:41:20 +0000

    Validating CAR reading.

--- tests/test.cjs
+++ tests/test.cjs
@@ -1,8 +1,8 @@
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
 const bskyAppDID = 'did:plc:z72i7hdynmk6r22z27h6tvur';
-const retr0idDID = 'did:plc:vwzwgnygau7ed7b7wt5ux7y2';
 
 
 const indexScript = fs.readFileSync(path.resolve(__dirname, '../index.js'), 'utf-8');
@@ -16,9 +16,9 @@ console.log('  ' + bskyAppObj.parseTime);
 
 fs.writeFileSync(__dirname + '/bsky-app.json', JSON.stringify(bskyAppObj, null, 2));
 
-console.log('@retr0.id..');
-const retr0IdRepo = fs.readFileSync(__dirname + '/retr0-id.car');
-const retr0IdObj = readCAR(retr0idDID, retr0IdRepo);
-console.log('  ' + retr0IdObj.parseTime);
+const bskyAppJSON = JSON.parse(fs.readFileSync(__dirname + '/bsky-app.json', 'utf-8'));
 
-fs.writeFileSync(__dirname + '/retr0-id.json', JSON.stringify(retr0IdObj, null, 2));
+for (let i = 0; i < bskyAppObj.length; i++) {
+  console.log(' [' + i + '] ' + bskyAppObj[i].$type);
+  assert.deepEqual(bskyAppObj[i], bskyAppJSON[i]);
+}
